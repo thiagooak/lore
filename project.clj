8e09f0ecaaaf5c28ee0ce50c1d1856aa3a2d10e1
Thiago Carvalho <thiago.oak@gmail.com>
2025-03-30 09:12:59 -0300

    AOT

--- project.clj
+++ project.clj
@@ -5,9 +5,8 @@
                  [io.pedestal/pedestal.jetty "0.7.2"]
                  [org.slf4j/slf4j-simple     "2.0.17"]]
 
-  :main ^:skip-aot lore.core
+  :main lore.core
   :uberjar-name "lore-standalone.jar"
   :min-lein-version "2.0.0"
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+  :profiles {:uberjar {:aot :all}})
